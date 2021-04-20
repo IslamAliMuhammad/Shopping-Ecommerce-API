d72f07055d713899ec0ad3c3f1c1912204f93051
IslamAliMuhammad <islam.ali.muhammad@gmail.com>
2021-04-20 08:40:00 +0200

    Basic models, migrations, seeders, and controllers

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Order;
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'units',
+        'price',
+        'photo_path',
+    ];
+
+    public function orders(){
+        return $this->hasMay(Order::class);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@laravel.com';
+        $user->password = bcrypt('01143101020');
+        $user->is_admin = true;
+        $user->save();
+    }
+}
